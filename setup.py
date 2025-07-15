d5a66916550e04a748a7cdb64806fd65b9e766df
vishalvansraj17 <vraj.vishalvansraj17@gmail.com>
2025-07-15 21:13:29 +0530

    floer structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MeddyBuddy Bot Gen Ai',
+    version= '0.0.0',
+    author= 'Vishal Vansraj',
+    author_email= 'vraj.vishalvansraj17@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
